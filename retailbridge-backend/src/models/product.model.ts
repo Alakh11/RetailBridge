aa8875f508da17a652e6c9468713e7a91fb2013f
Alakh Chaturvedi <alakh1304@gmail.com>
2025-01-25 21:19:08 +0530

    Product Model

---
+++ retailbridge-backend/src/models/product.model.ts
@@ -0,0 +1,66 @@
+import {Model, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Product extends Model {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+  })
+  description?: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  price: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  categoryId: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  stock: number;
+
+  @property({
+    type: 'date',
+  })
+  createdAt?: string;
+
+  @property({
+    type: 'date',
+  })
+  updatedAt?: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Product>) {
+    super(data);
+  }
+}
+
+export interface ProductRelations {
+  // describe navigational properties here
+}
+
+export type ProductWithRelations = Product & ProductRelations;
